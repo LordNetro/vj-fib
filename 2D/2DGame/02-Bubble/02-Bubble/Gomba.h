264ca69f1e4beb522ca7fd23cd0aeb0da0782a33
LordNetro <xdmarcitos@hotmail.com>
2023-11-07 18:56:53 +0100

    NOW YES!

---
+++ 2D/2DGame/02-Bubble/02-Bubble/Gomba.h
@@ -0,0 +1,41 @@
+#ifndef _GOMBA_INCLUDE
+#define _GOMBA_INCLUDE
+
+
+#include "Sprite.h"
+#include "TileMap.h"
+
+
+// Gomba is basically a Sprite that represents the enemy. As such it has
+// all properties it needs to track its movement, jumping, and collisions.
+
+
+class Gomba
+{
+
+public:
+	void init(const glm::ivec2& tileMapPos, ShaderProgram& shaderProgram);
+	void update(int deltaTime);
+	void render();
+
+	void setTileMap(TileMap* tileMap);
+	void setPosition(const glm::vec2& pos);
+	glm::ivec2 posGomba;
+
+private:
+	bool bJumping;
+	glm::ivec2 tileMapDispl;
+	int jumpAngle, startY;
+	Texture spritesheet;
+	Sprite* sprite;
+	TileMap* map;
+	float speedX;    
+	float accelX;
+	float maxSpeedX; 
+
+};
+
+
+#endif // _GOMBA_INCLUDE
+
+
